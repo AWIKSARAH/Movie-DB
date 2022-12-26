73ae28c04ced056c7e27a8ccc828dc08c2475999
Sarah Awik <sarah@sarahaweek.sarahawik>
2022-12-26 10:29:04 +0200

    Step4

--- index.js
+++ index.js
@@ -3,20 +3,37 @@ const app = express();
 
 //  Create Simple API
 app.get("/test", (req, res) => {
-    res.json({ status: 200, message: "OK" });
-  });
+  res.json({ status: 200, message: "OK" });
+});
 
 //Let's complicate : Time
 app.get("/time", (req, res) => {
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    res.json({ status: 200, message: `${hours}:${minutes}` });
-  });
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  res.json({ status: 200, message: `${hours}:${minutes}` });
+});
 
-  
+//Hello API
+app.get("/hello/:id?", (req, res) => {
+  const id = req.params.id || "World";
+  res.json({ status: 200, message: `Hello, ${id}` });
+});
+
+// Search
+app.get("/search", (req, res) => {
+  const search = req.query.s;
+  if (search) {
+    res.status(200).json({ status: 200, message: "OK", data: search });
+  } else {
+    res.status(500).json({
+      status: 500,
+      error: true,
+      message: "You Don't have any things to search",
+    });
+  }
+});
 //Server Port
 app.listen(3000, () => {
-    console.log("Server listening on port 3000");
-  });
-  
+  console.log("Server listening on port 3000");
+});
