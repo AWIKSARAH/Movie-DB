9ed536b4cf205eff34a01425db9bc926b56266fc
Sarah Awik <sarah@sarahaweek.sarahawik>
2022-12-26 10:34:20 +0200

    Step5

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
 //  Create Simple API
 app.get("/test", (req, res) => {
   res.json({ status: 200, message: "OK" });
@@ -33,6 +40,30 @@ app.get("/search", (req, res) => {
     });
   }
 });
+
+//create a new search Movies
+app.get("/movies/create", (req, res) => {
+    res.send("This is the create movies route");
+  });
+
+//read
+  app.get("/movies/read", (req, res) => {
+    res.send({
+      status: 200,
+      message: "this list of movies has been",
+      data: movies,
+    });
+// Update
+app.get("/movies/update", (req, res) => {
+    res.send("This is the update movies route");
+  });
+
+  //Delete
+  app.get("/movies/delete", (req, res) => {
+    res.send("This is the delete movies route");
+  });
+
+  });
 //Server Port
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
